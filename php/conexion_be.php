a78c2e75c0786eac9c452b8dfbc75aa5067003d2
Juan Antonio Martin Panchon <martin.panchon.juan.antonio@iescamas.es>
2024-11-22 21:25:51 +0100

    realizado cambios

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+$conexion = mysqli_connect("localhost", "mypanchonb0", "22bN2m3e", "login_register_db");
 
 /*/
 if ($conexion){
